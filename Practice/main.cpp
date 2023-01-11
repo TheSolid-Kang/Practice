7d6ca90cfaeb69fb635b44ea15abcd207fc6f226
TheSolid-KANG <tortoise8947@gmail.com>
2023-01-11 10:39:44 +0900

    CTempFunction 기능 사용할 수 있도록 수정

--- Practice/main.cpp
+++ Practice/main.cpp
@@ -1,38 +1,83 @@
 #include "Headers.h"
 #include "CPracticeMgr.h"
 #include "CTempFunctionMgr.h"
-#include "Headers.h"
 #include "Enum.h"
 
-void PrintTitle();
-
+void PrintTitle(int _selected_mgr = 0);
+enum class SELECTED_MGR : int { PRACTICE = 1, TEMP_FUNCTION, END};
 int main() {
 #if UNICODE
 	std::locale::global(std::locale(".UTF-8"));
 #endif
-	auto& instance = (*CPracticeMgr::GetInstance());
-	do {
+	auto& practice_instance = (*CPracticeMgr::GetInstance());
+	auto& tfunction_instance = (*CTempFunctionMgr::GetInstance());
+	int selected_mgr = 0;
+	while (EXIT != selected_mgr)
+	{
 		PrintTitle();
-		int num = CIO::ask_and_return_integer();
-		if (num == EXIT)
-			break;
-		instance.SetPractice(num);
-	} while (instance.Execute() != -1); 
+		selected_mgr = CIO::ask_and_return_integer();
 
-	instance.DestroyInstance();
-	(*CTempFunctionMgr::GetInstance()).DestroyInstance();
+		if (EXIT == selected_mgr)
+			break;
+		else if (static_cast<int>(SELECTED_MGR::PRACTICE) == selected_mgr)
+		{
+			do {
+				PrintTitle(selected_mgr);
+				int num = CIO::ask_and_return_integer();
+				if (EXIT == num)
+					break;
+				practice_instance.SetPractice(num);
+			} while (practice_instance.Execute() != -1);
+		}
+		else if(static_cast<int>(SELECTED_MGR::TEMP_FUNCTION) == selected_mgr)
+		{
+			do {
+				PrintTitle(selected_mgr);
+				int num = CIO::ask_and_return_integer();
+				if (EXIT == num)
+					break;
+				tfunction_instance.SetTempFunction(num);
+			} while (tfunction_instance.Execute() != -1);
+		}
+	}
+	practice_instance.DestroyInstance();
+	tfunction_instance.DestroyInstance();
 	return EXIT_SUCCESS;
 }
 
-void PrintTitle()
+void PrintTitle(int _selected_mgr)
 {
 	std::list<TString> list_title;
 
-	list_title.emplace_back(_T("1. Practice 1. baekjoon 문제지 출력 구상"));
-	list_title.emplace_back(_T("2. Practice 2. "));
-	list_title.emplace_back(_T(""));
-	list_title.emplace_back(_T(""));
-	list_title.emplace_back(_T("99.EXIT"));
 
+	if (static_cast<int>(SELECTED_MGR::PRACTICE) == _selected_mgr)
+	{
+		list_title.emplace_back(_T(""));
+		list_title.emplace_back(_T("1. Practice: "));
+		list_title.emplace_back(_T("2. Practice: "));
+		list_title.emplace_back(_T(""));
+		list_title.emplace_back(_T(""));
+		list_title.emplace_back(_T("99.EXIT"));
+	}
+	else if (static_cast<int>(SELECTED_MGR::TEMP_FUNCTION) == _selected_mgr)
+	{
+		list_title.emplace_back(_T(""));
+		list_title.emplace_back(_T("1. 파일 관련 함수 모음"));
+		list_title.emplace_back(_T("2. 코테 관련 함수 모음"));
+		list_title.emplace_back(_T("3. 서버 관련 함수 모음: 파이프 서버"));
+		list_title.emplace_back(_T(""));
+		list_title.emplace_back(_T("99.EXIT"));
+	}
+	else 
+	{
+		list_title.emplace_back(_T(""));
+		list_title.emplace_back(_T("1. Practice"));
+		list_title.emplace_back(_T("2. TempFunction"));
+		list_title.emplace_back(_T(""));
+		list_title.emplace_back(_T("99.EXIT"));
+	}
+
+	system("pause");
+	system("cls");
 	std::for_each(list_title.cbegin(), list_title.cend(), [](auto& _title) {std::tcout << _title << std::endl; });
 }
