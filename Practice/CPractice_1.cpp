b6c24b737fc7efeae732fe8690d3bfd95ec885d3
TheSolid-KANG <tortoise8947@gmail.com>
2023-01-27 00:13:27 +0900

    #6 forwardlist 구현 push_front() pop_front() sort() remove_if() remove() clear()

--- Practice/CPractice_1.cpp
+++ Practice/CPractice_1.cpp
@@ -1,6 +1,6 @@
+#include "CForwardList.hpp"
 #include "CPractice_1.h"
 #include <forward_list>
-#include "CForwardList.hpp"
 CPractice_1::CPractice_1()
 	: m_uniq_map_func(std::make_unique<std::map<size_t, std::function<std::shared_ptr<void>(const void*)>>>())
 	, m_uniq_map_testfunc(std::make_unique<std::map<size_t, std::function<std::shared_ptr<void>(const void*)>>>())
@@ -16,7 +16,7 @@ CPractice_1::~CPractice_1()
 void CPractice_1::_Initialize()
 {
 	auto arr_title = _BuildArray(_T(" ")
-		, _T("1. TCHAR 변환 연습 ")
+		, _T("1. foreard_list 예제")
 		, _T("2. ForwardList 구현")
 		, _T("")
 		, _T("99. EXIT")
@@ -73,14 +73,51 @@ void CPractice_1::_InitFunc(void)
 		[&](const void* _p_void) -> std::shared_ptr<void> {
 			std::forward_list<int> forwardList;
 			forwardList.push_front(1);
+			forwardList.push_front(0);
 			forwardList.push_front(1);
+			forwardList.push_front(1);
+			forwardList.push_front(2);
+			forwardList.push_front(2);
+			forwardList.push_front(2);
+			forwardList.push_front(2);
+			forwardList.push_front(3);
+			forwardList.push_front(3);
+			forwardList.push_front(3);
+			forwardList.push_front(1);
+
+			forwardList.pop_front();
+
+			forwardList.sort();
+
+			forwardList.remove(1);
+			forwardList.remove_if([](auto _first) -> bool { return _first >= 3 ? true : false; });
+
+			forwardList.clear();
 			return nullptr; }));
 	(*m_uniq_map_func).emplace(std::make_pair(static_cast<size_t>(FUNC::TWO),
 		[&](const void* _p_void) -> std::shared_ptr<void> {
-			CForwardList<int> forwardList1;
-			forwardList1.push_front(0);
-			forwardList1.push_front(1);
-			forwardList1.push_front(2);
+			CForwardList<int> forwardList;
+			forwardList.push_front(1);
+			forwardList.push_front(0);
+			forwardList.push_front(1);
+			forwardList.push_front(1);
+			forwardList.push_front(2);
+			forwardList.push_front(2);
+			forwardList.push_front(3);
+			forwardList.push_front(2);
+			forwardList.push_front(2);
+			forwardList.push_front(3);
+			forwardList.push_front(2);
+			forwardList.push_front(3);
+			forwardList.push_front(1);
+			auto num = forwardList.pop_front();
+			const auto num2 = forwardList.pop_front();
+			forwardList.push_front(forwardList.pop_front());
+
+			forwardList.sort();
+
+			forwardList.remove(1);
+			forwardList.remove_if([](auto _first) -> bool { return _first >= 3 ? true : false; });
 
 
 			return nullptr; }));
